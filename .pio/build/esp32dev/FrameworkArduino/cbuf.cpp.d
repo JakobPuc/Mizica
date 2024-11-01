2644287c41c763f54397148f0295bbfa3d8b0aec
Ama-dej <amadej.arh@gmail.com>
2024-11-01 19:29:02 +0100

    neki

--- .pio/build/esp32dev/FrameworkArduino/cbuf.cpp.d
+++
@@ -1,3 +0,0 @@
-.pio/build/esp32dev/FrameworkArduino/cbuf.cpp.o: \
- /home/aafedora/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
- /home/aafedora/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
